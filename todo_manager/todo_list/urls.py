8a5583a24b78c8284dac1c85c8d712ec01c9746a
Jeyw1n <j3ywin@gmail.com>
2024-12-07 23:13:01 +0300

    Added links to the form for adding ToDo Items

--- todo_manager/todo_list/urls.py
+++ todo_manager/todo_list/urls.py
@@ -8,5 +8,6 @@ app_name = 'todo_list'
 urlpatterns = [
     path('', views.ToDoListIndexView.as_view(), name='index'),
     path('done/', views.ToDoListDoneView.as_view(), name='done'),
-    path('<int:pk>/', views.ToDoDetailView.as_view(), name='detail')
+    path('<int:pk>/', views.ToDoDetailView.as_view(), name='detail'),
+    path('create/', views.ToDoItemCreateView.as_view(), name='create'),
 ]
